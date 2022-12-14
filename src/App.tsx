f7a946d81c80ed9f0e971109b175144ac144ce30
Maksym Shokin <problaze7@gmail.com>
2022-12-14 16:20:45 +0200

    leva

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import { Canvas } from '@react-three/fiber';
 import './App.css';
 import { Objects } from './components/Objects/Objects';
 import { CustomObject } from './components/CustomObject/CustomObject';
+import { Leva } from 'leva';
 
 const App = () => {
   return (
     <div className='App'>
+      <Leva collapsed />
       <Canvas
-        // defaults
-        // dpr={[1, 2]}
-        // gl={{
-        //   antialias: true
-        // }}
-        // camera={{
-        //   fov: 45,
-        //   near: 0.1,
-        //   far: 200,
-        //   position: [3, 2, 6]
-        // }}
+      // defaults
+      // dpr={[1, 2]}
+      // gl={{
+      //   antialias: true
+      // }}
+      // camera={{
+      //   fov: 45,
+      //   near: 0.1,
+      //   far: 200,
+      //   position: [3, 2, 6]
+      // }}
       >
         <Objects />
         {/* <CustomObject /> */}
--- src/components/Objects/Objects.tsx
+++ src/components/Objects/Objects.tsx
@@ -12,11 +12,40 @@ import {
 } from '@react-three/drei';
 import styles from './objects.module.css';
 import font from '../../assets/bangers-v20-latin-regular.woff';
+import { useControls, button } from 'leva';
 
 export const Objects = () => {
   const sphereRef = useRef<Mesh>(null);
   const boxRef = useRef<Mesh>(null);
   const groupRef = useRef<Group>(null);
+
+  const { cubeScale, cubeVisible } = useControls('cube', {
+    cubeScale: {
+      value: 1,
+      min: 1,
+      max: 7,
+      step: 0.5
+    },
+    cubeVisible: true,
+    clickMe: button(() => {
+      console.log('clicked');
+    }),
+    select: { options: ['x', 'y', 'z'] },
+    range: {
+      min: 0,
+      max: 10,
+      value: [4, 5]
+    }
+  });
+
+  const { spherePosition, color } = useControls('sphere', {
+    spherePosition: {
+      value: { x: -2, y: 0 },
+      joystick: 'invertY',
+      step: 0.01
+    },
+    color: 'orange'
+  });
 
   useFrame((state, delta) => {
     // if (boxRef?.current?.rotation) {
@@ -40,33 +69,45 @@ export const Objects = () => {
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
       <group ref={groupRef}>
-        <PivotControls anchor={[0, 0, 0]} depthTest={false}>
-          <mesh position-x={-2} ref={sphereRef}>
-            <sphereGeometry />
-            <meshStandardMaterial color='orange' />
-            <Html
-              position={[0.5, 0, 0]}
-              center
-              distanceFactor={8}
-              wrapperClass={styles.label}
-              occlude={[boxRef]}
-            >
-              This is a Sphere
-            </Html>
-          </mesh>
-        </PivotControls>
+        {/* <PivotControls anchor={[0, 0, 0]} depthTest={false}> */}
+        <mesh
+          position={[spherePosition.x, spherePosition.y, 0]}
+          ref={sphereRef}
+        >
+          <sphereGeometry />
+          <meshStandardMaterial color={color} />
+          {/* <Html
+            position={[0.5, 0, 0]}
+            center
+            distanceFactor={8}
+            wrapperClass={styles.label}
+            occlude={[boxRef]}
+          >
+            This is a Sphere
+          </Html> */}
+        </mesh>
+        {/* </PivotControls> */}
 
-        <TransformControls position-x={2}>
-          <mesh ref={boxRef}>
-            <boxGeometry />
-            <meshStandardMaterial color='brown' />
-          </mesh>
-        </TransformControls>
+        {/* <TransformControls position-x={2}> */}
+        <mesh
+          position-x={2}
+          visible={cubeVisible}
+          ref={boxRef}
+          scale={cubeScale}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color='brown' />
+        </mesh>
+        {/* </TransformControls> */}
       </group>
 
       <mesh rotation-x={-Math.PI * 0.5} scale={10} position={[0, -1, 0]}>
         <planeGeometry />
-        <MeshReflectorMaterial resolution={1024} mirror={0.75} color='greenyellow' />
+        <MeshReflectorMaterial
+          resolution={1024}
+          mirror={0.75}
+          color='greenyellow'
+        />
         {/* <meshStandardMaterial color='green' /> */}
       </mesh>
 
